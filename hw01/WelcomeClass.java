b6e501e9bde982452567d3207f6f31fb0316ad46
Marc Laflamme <mrl218@lehigh.edu>
2015-01-24 19:13:50 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,23 @@
+//Marc Laflamme
+// Welcome Class Program
+// compile javac WelcomeClass.java, then run program java WelcomeClass//
+
+//define a class
+public class WelcomeClass{
+    
+    //add main method
+    public static void main(String[] args) {
+        //print the statements with surrounding shapes
+        System.out.println("     -----------      ");
+        System.out.println("     | WELCOME |      ");
+        System.out.println("     -----------      ");
+        System.out.println("   ^  ^  ^  ^  ^  ^   ");
+        System.out.println("  / \\/ \\/ \\/ \\/ \\/ \\  ");
+        System.out.println(" <-M--R--L--2--1--8-> ");
+        System.out.println("  \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("   v  v  v  v  v  v   ");
+        System.out.println(" This past summer my best friends and I went " );
+        System.out.println(" on a gradutation road trip to Canada." );
+        //test/test
+    }
+}
