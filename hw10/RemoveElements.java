629dbebd7be19efbdcbe25ccff9ce5ba85a4a8ca
Marc Laflamme <mrl218@lehigh.edu>
2015-04-10 01:01:15 +0000

    Updated RemoveElements.java

---
+++ hw10/RemoveElements.java
@@ -0,0 +1,73 @@
+//Marc Laflamme
+// CSE2 HW 10 RemoveElements.java
+// April 8 2015
+// Write a program using arrays to complete the methods 
+//for the given main method
+
+import java.util.Scanner;
+public class RemoveElements{
+  public static void main(String [] arg){
+	Scanner scan=new Scanner(System.in);
+int num[]=new int[10];
+int newArray1[];
+int newArray2[];
+int index,target;
+	String answer="";
+	do{
+  	System.out.print("Random input 10 ints [0-9]");
+  	num = randomInput();
+  	String out = "The original array is:";
+  	out += listArray(num);
+  	System.out.println(out);
+ 
+  	System.out.print("Enter the index ");
+  	index = scan.nextInt();
+  	newArray1 = delete(num,index);
+  	String out1="The output array is ";
+  	out1+=listArray(newArray1); //return a string of the form "{2, 3, -9}"  
+  	System.out.println(out1);
+ 
+      System.out.print("Enter the target value ");
+  	target = scan.nextInt();
+  	newArray2 = remove(num,target);
+  	String out2="The output array is ";
+  	out2+=listArray(newArray2); //return a string of the form "{2, 3, -9}"  
+  	System.out.println(out2);
+  	 
+  	System.out.print("Go again? Enter 'y' or 'Y', anything else to quit-");
+  	answer=scan.next();
+	}while(answer.equals("Y") || answer.equals("y"));
+  }
+ 
+  public static String listArray(int num[]){
+	String out="{";
+	for(int j=0;j<num.length;j++){
+  	if(j>0){
+    	out+=", ";
+  	}
+  	out+=num[j];
+	}
+	out+="} ";
+	return out;
+  }
+  
+  public static int randomInput(){
+  	Random rand = new Random();
+     int  randomArray[]= rand.nextInt(9) + 0;
+     return randomArray;
+}
+public static int delete(list,pos) {
+	list.splice(pos); // delete the component of the array at position pos
+	return list; // return the new modified list array
+}
+public static int remove(list,target){
+	while (list==target){
+		list.splice(pos); // delete the component of the array at position pos
+	}
+	
+}
+}
+
+
+
+ 
